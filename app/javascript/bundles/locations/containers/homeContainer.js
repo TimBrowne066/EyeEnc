85561ce2efb46eae29019b1db6b4a66b245065e9
Timothy Browne <timbrowne066@gmail.com>
2017-10-09 23:03:59 -0400

    sass

--- app/javascript/bundles/locations/containers/homeContainer.js
+++ app/javascript/bundles/locations/containers/homeContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navbar, NavItem, MenuItem, NavDropdown, Nav, PanelGroup, Panel, Grid, Row, Col, Button, Well } from 'react-bootstrap';
 
 class HomeContainer extends React.Component {
   constructor(props) {
@@ -11,7 +12,31 @@ class HomeContainer extends React.Component {
   render() {
 
     return (
-      <h1>Hello from React</h1>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Eye Encounters</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
