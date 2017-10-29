8b7316e58b7fe53da68be13173d6b445686ffe6f
Timothy Browne <timbrowne066@gmail.com>
2017-10-28 21:56:57 -0400

    5 minute ping because to hell with heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,5 @@
 
 var http = require("http");
 setInterval(function() {
-    http.get("http://<your app name>.herokuapp.com");
+    http.get("http://eyeencounters.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
