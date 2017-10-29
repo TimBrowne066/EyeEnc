2cf5d3550142c301ec33827a1c979689a6315451
Timothy Browne <timbrowne066@gmail.com>
2017-10-28 21:32:47 -0400

    added ping script to prevent heroku app sleep

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,8 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
